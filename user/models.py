5ae6254b13e234125c7df4750e9a958c8b112178
Robson J <robsonjrservices@gmail.com>
2023-12-10 14:15:40 -0300

    FEAT: Models with input fields created and configured, in addition to migrations performed

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(primary_key=True, max_length=200)
+    full_name = models.CharField(max_length=300)
+    email = models.EmailField()
+    password = models.CharField(max_length=128)
