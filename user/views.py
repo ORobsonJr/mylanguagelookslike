878521268acc30ae94ed20db8fbd6915318e0745
Robson J <robsonjrservices@gmail.com>
2023-12-11 16:07:27 -0300

    FEAT: New application in views to search for users via username

--- user/views.py
+++ user/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import User
+from django.views.decorators.http import require_GET
+
 
 # Create your views here.
+
+@require_GET
+def find_user_by_username(request):
+    try:
+        username_ = request.GET.get('username')
+        user = User.objects.get(username=username_)
+    
+        return user
+
+    
+    except Exception as e:
+        return e
+    
+    
